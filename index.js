c7e8b72300001f13ea045a6be31aeab355dea4ab
poojapriya-pakal <134307795+poojapriya-pakal@users.noreply.github.com>
2024-08-25 17:28:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,62 +1,30 @@
-'use strict';
-module.exports = balanced;
-function balanced(a, b, str) {
-  if (a instanceof RegExp) a = maybeMatch(a, str);
-  if (b instanceof RegExp) b = maybeMatch(b, str);
+"use strict";
+const pattern = /-(\w|$)/g;
 
-  var r = range(a, b, str);
+const callback = (dashChar, char) => char.toUpperCase();
 
-  return r && {
-    start: r[0],
-    end: r[1],
-    pre: str.slice(0, r[0]),
-    body: str.slice(r[0] + a.length, r[1]),
-    post: str.slice(r[1] + b.length)
-  };
-}
 
-function maybeMatch(reg, str) {
-  var m = str.match(reg);
-  return m ? m[0] : null;
-}
 
-balanced.range = range;
-function range(a, b, str) {
-  var begs, beg, left, right, result;
-  var ai = str.indexOf(a);
-  var bi = str.indexOf(b, ai + 1);
-  var i = ai;
+const camelCaseCSS = property =>
+{
+	property = property.toLowerCase();
 
-  if (ai >= 0 && bi > 0) {
-    if(a===b) {
-      return [ai, bi];
-    }
-    begs = [];
-    left = str.length;
+	// NOTE :: IE8's "styleFloat" is intentionally not supported
+	if (property === "float")
+	{
+		return "cssFloat";
+	}
+	// Microsoft vendor-prefixes are uniquely cased
+	else if (property.startsWith("-ms-"))
+	{
+		return property.substr(1).replace(pattern, callback);
+	}
+	else
+	{
+		return property.replace(pattern, callback);
+	}
+};
 
-    while (i >= 0 && !result) {
-      if (i == ai) {
-        begs.push(i);
-        ai = str.indexOf(a, i + 1);
-      } else if (begs.length == 1) {
-        result = [ begs.pop(), bi ];
-      } else {
-        beg = begs.pop();
-        if (beg < left) {
-          left = beg;
-          right = bi;
-        }
 
-        bi = str.indexOf(b, i + 1);
-      }
 
-      i = ai < bi && ai >= 0 ? ai : bi;
-    }
-
-    if (begs.length) {
-      result = [ left, right ];
-    }
-  }
-
-  return result;
-}
+module.exports = camelCaseCSS;
